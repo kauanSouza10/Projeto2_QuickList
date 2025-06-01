f8ec18f4ee1b9783a624f3579fe647e6acf04856
Kauan Ferreira de Souza <kauan.souza@eaportal.org>
2025-06-01 21:51:53 +0000

    criação da função para remover os itens do array:removeItems

--- main.js
+++ main.js
@@ -1,16 +1,39 @@
-const items = []
+const items = [];
 
 function addItem() {
-    const itremName = document.querySelector("#item").value
+    const itemName = document.querySelector("#item").value;
 
     const item = {
-      name: itremName,
+      name: itemName,
       checked: false  
-    }
+    };
 
     items.push(item)
 
-    document.querySelector("#item").value ""
+    document.querySelector("#item").value = "";
 
-    console.log(items)
+    showItemsList()
+}
+
+function showItemsList() {
+    const sectionList = document.querySelector(".list");
+    sectionList.textContent = ""; 
+
+    items.map((item, index) => {
+        sectionList.innerHTML += `
+         <div class="item">
+          <div>
+            <input type="checkbox" name="List" id="item-${index}">
+            <div class="custom-checkbox">
+              <img src="./assets/checked.svg" alt="checked">
+            </div>
+            <label for="item-${index}">${item.name}</label>
+          </div>
+
+          <button>
+            <img src="./assets/trash-icon.svg" alt="trash icon">
+          </button>
+        </div>
+        `;
+    });
 }
